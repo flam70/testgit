93e593c199ce08425c1586296afcb007b79595a4
Franck <franck.lamy@qualims.com>
2018-06-22 11:58:07 +0200

    Ajout Marabout 3 phrases

---
+++ src/main/java/com/qualims/testgit/Marabout.java
@@ -0,0 +1,11 @@
+package com.qualims.testgit;
+
+public class Marabout {
+
+	public static String resultat() {
+		return "Trois p'tits chats ..., \n" 
+				+ "Chapeau de paille ..., \n"
+				+ "Paillasson ...";
+	}
+
+}
