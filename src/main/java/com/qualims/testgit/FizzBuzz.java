0bc67ad22c6123c612ce75b1595a8f9517c01de5
Franck <franck.lamy@qualims.com>
2018-06-22 11:15:53 +0200

    Ajout FizzBuzz de 1 à 15

---
+++ src/main/java/com/qualims/testgit/FizzBuzz.java
@@ -0,0 +1,10 @@
+package com.qualims.testgit;
+
+public class FizzBuzz {
+
+	public static String resultat() {
+		
+		return "1,2,Fizz,4,Buzz,7,8,Fizz,Buzz,11,Fizz,13,14,Fizz Buzz";
+	}
+
+}
--- src/main/java/com/qualims/testgit/HelloWorld.java
+++ src/main/java/com/qualims/testgit/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 		System.out.println("Jouons ensemble!");
+		
+		System.out.println("Et si on jouait au FizzBuzz ?");
+		System.out.println(FizzBuzz.resultat());
+		
 	}
 
 }
