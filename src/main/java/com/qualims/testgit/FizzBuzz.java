97f7eec824b8a330df4eea49cb88103b7f62790c
Franck <franck.lamy@qualims.com>
2018-06-22 12:09:44 +0200

    Suite FizzBuzz de 22 à 25

--- src/main/java/com/qualims/testgit/FizzBuzz.java
+++ src/main/java/com/qualims/testgit/FizzBuzz.java
@@ -5,7 +5,8 @@ public class FizzBuzz {
 	public static String resultat() {
 		
 		return "1,2,Fizz,4,Buzz,7,8,Fizz,Buzz,11,Fizz,13,14,Fizz Buzz"
-				+ ",16,17,Fizz,19,Buzz,Fizz";
+				+ ",16,17,Fizz,19,Buzz,Fizz"
+				+",22,23,Fizz,Buzz";
 	}
 
 }
