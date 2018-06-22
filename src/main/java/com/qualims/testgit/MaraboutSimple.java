f7b7d8430d9165d6b8330f5015672abd0e6e2d9d
Franck <franck.lamy@qualims.com>
2018-06-22 14:15:26 +0200

    Ajout Marabout simple 3 mots de plus

--- src/main/java/com/qualims/testgit/MaraboutSimple.java
+++ src/main/java/com/qualims/testgit/MaraboutSimple.java
@@ -3,7 +3,8 @@ package com.qualims.testgit;
 public class MaraboutSimple {
 
 	public static String resultat() {
-		return "Marabout, Bout de ficelle, Selle de cheval";
+		return "Marabout, Bout de ficelle, Selle de cheval"
+				+ ", Cheval de course, Course à pied, Pied à terre";
 	}
 
 }
